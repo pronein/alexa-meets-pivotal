de11a01630da3fb6628551ea0f2256283989a1fa
adam-schrader <adam.schrader@deluxe.com>
2017-11-07 01:18:26 -0500

    Move common functionality to lib
    Clean up logging

--- functions/__main__.js
+++ functions/__main__.js
@@ -7,13 +7,9 @@ const lib = require('lib');
 * @returns {any}
 */
 module.exports = (name = '', slots = {}, request = {}, context, callback) => {
-
-  console.log(`Name: ${name}`);
-  console.log(`Slots: ${JSON.stringify(slots)}`);
   request.intent = request.intent || {
     name: name,
     slots: Object.keys(slots).reduce((obj, key) => {
-      console.log(`Obj: ${JSON.stringify(obj)} - Key: ${JSON.stringify(key)}`);
       obj[key] = (slots[key] && typeof slots[key] === 'object') ?
         slots[key] : {name: key, value: slots[key]};
       return obj[key];
@@ -24,13 +20,10 @@ module.exports = (name = '', slots = {}, request = {}, context, callback) => {
     return callback(new Error('Intent name is required'));
   }
 
-  console.log(`Request.Intent.Slots: ${JSON.stringify(request.intent.slots)}`);
   let params = Object.keys(request.intent.slots || {}).reduce((params, key) => {
     params[key] = request.intent.slots[key].value;
     return params;
   }, {});
-
-  console.log(`params: ${JSON.stringify(params)}`);
 
   lib[`${context.service.identifier}.intents.${request.intent.name}`](params, (err, result) => {
 
--- functions/intents/AlexaMeetsPivotal.js
+++ functions/intents/AlexaMeetsPivotal.js
@@ -1,235 +1,46 @@
+'use strict';
+
 const lib = require('lib');
-//const pivotal = require('../pivotalapi');
-const request = require('request');
 const pivotal = require('../../lib/pivotal');
 
-const common = {
-  apiToken: '',
-  apiVersion: '5',
-  apiTokenHeader: 'X-TrackerToken'
-};
-
-const rootUri = 'https://www.pivotaltracker.com/services/v' + common.apiVersion;
-
-function Api() {
-  this.getCurrentIterationId = function (projectId, cb) {
-
-    const uri = [
-      rootUri,
-      Api.prototype.uris.projects,
-      projectId
-    ].join('/');
-
-    const opts = {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      url: uri,
-      method: 'GET'
-    };
-
-    opts.headers[common.apiTokenHeader] = common.apiToken;
-
-    request(opts, function (err, res, data) {
-      if (err) {
-        return cb(err);
-      }
-
-      const parsed = JSON.parse(data);
-
-      return cb(null, {
-        currentIteration: parsed['current_iteration_number'],
-        currentVelocity: parsed['velocity_averaged_over']
-      });
-    });
-  };
-
-  this.getIteration = function (projectId, iterationId, cb) {
-    console.log(`Project Id: ${projectId}`);
-    console.log(`Iteration Id: ${iterationId}`);
-
-    const uri = [
-      rootUri,
-      Api.prototype.uris.projects,
-      projectId,
-      Api.prototype.uris.iterations,
-      iterationId
-    ].join('/');
-
-    const opts = {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      url: uri + '?fields=',
-      method: 'GET',
-      qs: {
-        fields: 'number,velocity,stories(id,name,story_type,estimate,current_state,tasks(description,complete))'
-      }
-    };
-
-    opts.headers[common.apiTokenHeader] = common.apiToken;
-
-    console.log(`Uri: ${uri}`);
-    console.log(`Opts: ${JSON.stringify(opts)}`);
-
-    request(opts, function (err, res, data) {
-      if (err) {
-        return cb(err);
-      }
-
-      const parsed = JSON.parse(data);
-      const mapped = parsed.stories
-        .filter(function (story) {
-          return /^\*{2}\s?\^/.exec(story.name.trim()) === null;
-        })
-        .map(function (story) {
-          return {
-            id: story.id,
-            type: story['story_type'],
-            state: story['current_state'],
-            points: story.estimate === undefined ? '-' : story.estimate,
-            tasks: story.tasks.map(function (task) {
-              return {
-                id: task.id,
-                isComplete: task.complete,
-                department: getTaskDepartment(task.description),
-                hours: getTaskHours(task.description)
-              };
-            })
-          };
-        });
-
-      const completeStories = mapped.filter(function (m) {
-        return m.state === 'accepted';
-      });
-      const totalPoints = mapped.reduce(function (agg, m) {
-        return agg + (m.points !== '-' ? parseInt(m.points) : 0);
-      }, 0);
-      const completePoints = completeStories.reduce(function (agg, m) {
-        return agg + (m.points !== '-' ? parseInt(m.points) : 0);
-      }, 0);
-      const totalHoursDev = mapped.reduce(function (agg, m) {
-        return agg + m.tasks.reduce(function (tagg, t) {
-            return tagg + (t.department === 'DEV' ? t.hours : 0);
-          }, 0);
-      }, 0);
-      const totalHoursDevComplete = mapped.reduce(function (agg, m) {
-        return agg + m.tasks.reduce(function (tagg, t) {
-            return tagg + (t.department === 'DEV' && t.isComplete ? t.hours : 0);
-          }, 0);
-      }, 0);
-      const totalHoursQa = mapped.reduce(function (agg, m) {
-        return agg + m.tasks.reduce(function (tagg, t) {
-            return tagg + (t.department === 'QA' ? t.hours : 0);
-          }, 0);
-      }, 0);
-      const totalHoursQaComplete = mapped.reduce(function (agg, m) {
-        return agg + m.tasks.reduce(function (tagg, t) {
-            return tagg + (t.department === 'QA' && t.isComplete ? t.hours : 0);
-          }, 0);
-      }, 0);
-
-      return cb(null, {
-        velocity: parsed.velocity,
-        totalStories: mapped.length,
-        completeStories: completeStories.length,
-        totalPoints: totalPoints,
-        completePoints: completePoints,
-        totalHours: totalHoursDev + totalHoursQa,
-        totalHoursDev: totalHoursDev,
-        totalHoursQa: totalHoursQa,
-        completeHours: totalHoursDevComplete + totalHoursQaComplete,
-        completeHoursDev: totalHoursDevComplete,
-        completeHoursQa: totalHoursQaComplete,
-        stories: mapped
-      });
-    });
-  };
-
-  function getTaskDepartment(description) {
-    const lowered = description.toLowerCase().trim();
-    const deptRegex = /^(dev|qa)-?/;
-
-    const match = deptRegex.exec(lowered);
-    if (match) {
-      return match[1].toUpperCase();
-    }
-
-    return 'NONE';
-  }
-
-  function getTaskHours(description) {
-    const lowered = description.toLowerCase().trim();
-    const hoursRegex = /\((\d+\.?\d*?)hr?\)$/;
-
-    const match = hoursRegex.exec(lowered);
-    if (match) {
-      return parseFloat(match[1]);
-    }
-
-    return 0;
-  }
-}
-
-Api.prototype.uris = {
-  projects: 'projects',
-  iterations: 'iterations'
-};
-
-Api.prototype.projects = {
-  'Bankers Dashboard': 0
-};
+const common = new pivotal.Common();
+const api = new pivotal.Api(common.apiToken);
 
 /**
- * Basic "Hello World" intent, can receive a `name` parameter
- *   Call using $ lib . AlexaMeetsPivotal '{"slots":{"project":{"value":"<ProjectName>"},"iteration":{"value":<SprintNumber>}}}'
+ * Call using $ lib . AlexaMeetsPivotal '{"slots":{"project":{"value":"<ProjectName>"},"iteration":{"value":<SprintNumber>}}}'
  * @param {string} project Requested Deluxe project
- * @param {number} iteration Requested iteration of project (optional)
+ * @param {number} iterationId Requested iteration of project (optional)
  * @returns {string}
  */
-module.exports = (project = '', iteration = 0, callback) => {
+module.exports = (project = '', iterationId = 0, callback) => {
 
-  //let apiToken = '';
-  //let client = new tracker.Client(apiToken);
-  let api = new Api();
-  let projectId = api.projects[project];
-  const newApi = new pivotal.Api(common.apiToken);
+  const projectId = common.getProjectId(project);
 
-  newApi.getCurrentIterationId(projectId, function (err, iterationId) {
-    console.log(`getCurrentIterationid:: ${JSON.stringify(err ? err : iterationId, null, 2)}`);
-    console.log(`Current Iteration #${iterationId}`);
+  if (!projectId) {
+    return callback(null, 'Sorry, but I could not find that project.');
+  }
 
-    api.getIteration(projectId, iteration || iterationId, function (err, iter) {
-      if (iter) delete iter.stories;
+  if (!iterationId) {
+    api.getCurrentIterationId(projectId, function (err, iterationId) {
+      if (err){
+        return callback(err);
+      }
 
-      newApi.getIteration(projectId, iteration || iterationId, function(errNew, iterationObj) {
-        console.log('Api Iteration Data:');
-        console.log(JSON.stringify(err ? err : iter, null, 2));
-        console.log('NewApi Iteration Data:');
-        const sprintStoryCount = iterationObj.getTotalStories(false, true);
-        const prodStoryCount = iterationObj.getTotalStories();
-        const techDebtStoryCount = sprintStoryCount - prodStoryCount;
-        const bugBashStoryCount = iterationObj.getTotalStories(false, true, true) - sprintStoryCount;
+      api.getIteration(projectId, iterationId, function (err, iteration) {
+        if (err) {
+          return callback(err);
+        }
 
-        console.log(JSON.stringify(errNew ? errNew : {
-          velocity: iterationObj.velocity,
-          sprintStories: sprintStoryCount,
-          completeStories: iterationObj.getTotalStories(true, true),
-          sprintProdStories: prodStoryCount,
-          sprintTechDebtStories: techDebtStoryCount,
-          sprintBugBashStories: bugBashStoryCount,
-          totalPoints: iterationObj.getTotalPoints(),
-          completePoints: iterationObj.getTotalPoints(true),
-          totalHours: iterationObj.getTotalHours(null, false, true),
-          totalHoursDev: iterationObj.getTotalHours('DEV', false, true),
-          totalHoursQa: iterationObj.getTotalHours('QA', false, true),
-          completeHours: iterationObj.getTotalHours(null, true, true),
-          completeHoursDev: iterationObj.getTotalHours('DEV', true, true),
-          completeHoursQa: iterationObj.getTotalHours('QA', true, true)
-        }, null, 2));
-
-        return callback(null, `Project ${project} [${iteration || iterationId}]`);
+        return callback(null, common.buildAlexaResponseFromIteration(project, iteration));
       });
     });
-  });
+  } else {
+    api.getIteration(projectId, iterationId, function (err, iteration) {
+      if(err) {
+        return callback(err);
+      }
+
+      return callback(null, common.buildAlexaResponseFromIteration(project, iteration));
+    });
+  }
 };
